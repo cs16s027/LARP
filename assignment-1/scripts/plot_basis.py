2cd1f1650b1c02b73cdf498e86c6490898ac0ce9
alinalbook <alinalbook@gmail.com>
2017-09-27 09:54:51 +0530

    Done

--- assignment-1/scripts/plot_basis.py
+++ assignment-1/scripts/plot_basis.py
@@ -2,13 +2,22 @@ import numpy as np
 from scipy.io import loadmat
 import cv2
 
+# Function to plot the basis
 def plotBasis(eigbasis):
+    # Plotting just 100 elements from the eigenbasis
     for i in range(100):
+        # Get the image - the transpose is necessary because the 
+        # order of flattening was different while getting the 
+        # eigenvectors
         image = eigbasis[i].reshape([92, 92]).T
+        # Normalize image
         image = image / np.max(image) * 255
+        # Write image to disk
         cv2.imwrite('eigfaces/eigface-%s.jpg' % str(i), image)
 
 if __name__ == '__main__':
+    # Load the eigenbasis
     eigbasis = loadmat('data/EigenVectors.mat')['COEFF']
+    # Plot the eigenbasis
     plotBasis(eigbasis)
 
--- assignment-1/scripts/project_image.py
+++ assignment-1/scripts/project_image.py
@@ -11,19 +11,32 @@ def norm(A):
             val += np.float32(A[x][y]) * np.float32(A[x][y])
     return np.sqrt(val)
 
+# Retrurns the relative error between image and reconstruction
 def relativeError(image, recon):
     return norm(recon - image)
 
+# Projects the image on to the eigenbasis and gets a reconstruction
 def projectImage(eigbasis, image, image_name):
+    # Image norm
     image_norm = norm(image)
+    # Flatten the image to get a vector. Transpose is necessary for
+    # compatability with eigenbasis column ordering.
     image_vector = image.T.flatten().reshape([1, -1])
+    # Get the projections on to the eigenbasis (eigenbasis is orthogonal)
     projections = np.dot(image_vector, eigbasis).flatten()
+    # Sort projections according to the magnitude of the contributions
     indices = np.argsort(np.abs(projections))[::-1]
+    # Reconstruction vector
     recon_vector = np.zeros((8464, ))
+    # Loop through all eiegenvectors in the basis
     for i in np.arange(0, recon_vector.shape[0], 1):
+        # Index of the ith most contributing eigenvector
         index = indices[i]
+        # Add this vector to the reconstruction
         recon_vector += projections[index] * eigbasis[:, index]
+        # This is the top-k eigenvector reconstruction
         recon_image = recon_vector.reshape((92, 92)).T
+        # Relative error between error image and original image
         error = relativeError(image, recon_image) / image_norm
         if error < 0.01:
             print 'Relative error drops below 0.01 at %s eigenfaces for %s' % (str(i + 1), image_name)
@@ -33,14 +46,20 @@ def projectImage(eigbasis, image, image_name):
     return False, '0'
 
 if __name__ == '__main__':
+    # Load the eigenbasis
     eigbasis = loadmat('data/EigenVectors.mat')['COEFF']
+    # File to store the top-k values for each image
     topk = open('reconstructions/top-k.txt', 'w')
+    # Loop through all 10 images
     for i in np.arange(1, 11, 1):
         image_name = 'face%d.pgm' % i
         image_path = 'data/images/%s' % image_name
         print 'Projection and reconstruction for %s' % image_name
+        # Load the image
         image = cv2.imread(image_path, 0)
+        # Get the reconstruction and top-k values
         recon, eig = projectImage(eigbasis, image, image_name)
+        # Write results to disk
         if recon is not False:
             cv2.imwrite('reconstructions/%s' % image_name, recon)
             topk.write('%s,%s\n' % (image_name, eig))
